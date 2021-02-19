06871bb4ec866197208c409fa37976b266d019b2
unknown <D00218148@student.dkit.ie>
2021-02-19 14:50:57 +0000

    More Updates

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,10 +1,37 @@
 import React from "react"
+import { Link } from "gatsby"
 import Container from "../components/container"
+import styles from "./about-css-modules.module.css"
+
+console.log(styles)
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+      
+    </div>
+  </div>
+)
+
 export default function About() {
   return (
     <Container>
+      <Link to="/">Home</Link>
       <h1>About CSS Modules</h1>
       <p>CSS Modules are cool</p>
+      <User
+        username="Van Darkholme"
+        avatar="https://pbs.twimg.com/profile_images/3764746483/443a214f86755e9488303b561a53ed6a_400x400.jpeg"
+        excerpt="There's a fine line between sex sound and taking a dump."
+      />
+      <User
+        username="Billy Herrington"
+        avatar="https://i.redd.it/2wu0kqfqibk41.jpg"
+        excerpt="Let us remember these timeless words: Ass♂we♂can."
+      />
     </Container>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Header from "../components/header"
 export default function Home() {
   return <div style={{ color: `#5beb34`}}>
     <Link to="/contact/">Contact</Link>
+    <Link to="/about-css-modules/">About</Link>
     <Header headerText="I have no sexual relations with that woman"/>
       <p style={{ color: `#00fbff`}}>This is fake news</p>
       <img src="https://media.tenor.com/images/3c04ce7ea3e696ba0502ad0b405ee4fb/tenor.gif" alt="RACCOON" width="1000" height="1000" />
