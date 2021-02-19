5fe4f5d95814aa09ac0ecae77708477c93d77d3a
unknown <D00218148@student.dkit.ie>
2021-02-19 13:52:12 +0000

    my amazing new website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div style={{ color: `#5beb34`}}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="I have no sexual relations with that woman"/>
+      <p style={{ color: `#00fbff`}}>This is fake news</p>
+      <img src="https://media.tenor.com/images/3c04ce7ea3e696ba0502ad0b405ee4fb/tenor.gif" alt="RACCOON" width="1000" height="1000" />
+      
+    </div>
 }
